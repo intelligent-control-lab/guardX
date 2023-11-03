6b6e995f81aabf8a49c07fe97be3953b3e635346
YIFANSUN98 <910720891syf@gmail.com>
2023-11-02 22:01:06 -0400

    fix reward bug with reset_done()

---
+++ safe_rl_libX/trpo/myTest.py
@@ -0,0 +1,46 @@
+from safe_rl_envs.envs.engine import Engine
+from safe_rl_envs.envs.mjx_device import device_put
+import numpy as np
+
+import torch
+import time
+import mediapy as media
+num_envs = 1
+config = {'num_envs':num_envs}
+env = Engine(config)
+obs = env.reset()
+
+t = time.time()
+print("start")
+images = []
+model_path = '/home/yifan/guardX/guardX/safe_rl_libX/trpo_guardX/logs/Goal_Point_8Hazards_trpo_kl0.02_epochs10_step400000/Goal_Point_8Hazards_trpo_kl0.02_epochs10_step400000_s0/pyt_save/model.pt'
+ac = torch.load(model_path)
+total_reward = 0
+for i in range(1000):
+    # act = np.random.uniform(-1,1,(num_envs, env.action_space.shape[0]))
+    # act = np.zeros(env.action_space.shape)
+    # act = torch.from_numpy(act).reshape(num_envs,-1)
+    act, v, logp, _, _ = ac.step(obs)
+    obs, reward, done, info = env.step(act)
+    total_reward += reward
+    env.render()
+    if done > 0:
+        obs = env.reset_done()
+        print(total_reward)
+        total_reward = 0
+        # import ipdb;ipdb.set_trace()
+    # images.append(env.render())
+print("finish ", time.time() - t)
+# print(obs.shape)
+# print(len(images), images[0].shape)
+# path = '/home/yifan/guardX/guardX/video.mp4'
+# media.write_video('/home/yifan/guardX/guardX/video.mp4', images, fps=60.0)
+
+# import jax
+# from jax import numpy as jp
+# import mujoco
+# from mujoco import mjx
+# # path = 'safe_rl_envs/safe_rl_envs/xmls/barkour_v0/assets/barkour_v0_mjx.xml'
+# path = 'safe_rl_envs/safe_rl_envs/xmls/barkour_v0/assets/barkour.xml'
+# mj_model = mujoco.MjModel.from_xml_path(path)
+# import ipdb;ipdb.set_trace()
